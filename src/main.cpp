fe81935ebb5e53f38e4c8f2031fde36e86209a90
Yourself <kyle.smith314@gmail.com>
2023-11-23 15:00:04 -0700

    add debug stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <iostream>
 
 int main(int argc, const char** argv) {
+    for (int i = 0; i < argc; ++i) {
+        std::cout << argv[i] << ' ';
+    }
+    std::cout << std::endl;
     auto result = parse(argc, argv);
     if (is_error(result)) {
         const auto& msg = std::get<Error>(result).msg;
