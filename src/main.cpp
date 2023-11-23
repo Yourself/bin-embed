3858d53d0ae07fbd56b59b5504006dd02321c053
Yourself <kyle.smith314@gmail.com>
2023-11-23 15:17:42 -0700

    remove debug stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,6 @@
 #include <iostream>
 
 int main(int argc, const char** argv) {
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << ' ';
-    }
     std::cout << std::endl;
     auto result = parse(argc, argv);
     if (is_error(result)) {
