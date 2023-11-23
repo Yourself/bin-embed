38e040365ca8b5eacc482699ced2c9619f0b87a0
Yourself <kyle.smith314@gmail.com>
2023-11-23 00:26:31 -0700

    more signed issues

--- src/writer.cpp
+++ src/writer.cpp
@@ -40,7 +40,7 @@ void writeStringSafeChar(std::ostream& os, char c) {
         if (std::isprint(static_cast<unsigned char>(c))) {
             os << c;
         } else {
-            os << "\\x" << std::hex << std::noshowbase << std::setfill('0') << std::setw(2) << static_cast<int>(c)
+            os << "\\x" << std::hex << std::noshowbase << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(c)
                << "\" \"";
         }
     }
